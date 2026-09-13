11931d35424ccdd5b07d78c9689847bc70570ef7
harshanimationsanimatorharsh-ai <harshanimationsanimatorharsh@gmail.com>
2026-09-13 18:01:13 +0530

    Update AiLabFragment.kt

--- app/src/main/java/com/vexo/app/ui/fragments/AiLabFragment.kt
+++ app/src/main/java/com/vexo/app/ui/fragments/AiLabFragment.kt
@@ -5,24 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.vexo.app.databinding.FragmentAilabBinding
+import com.vexo.app.R
 
 class AiLabFragment : Fragment() {
-
-    private var _binding: FragmentAilabBinding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentAilabBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_ailab, container, false)
     }
 }

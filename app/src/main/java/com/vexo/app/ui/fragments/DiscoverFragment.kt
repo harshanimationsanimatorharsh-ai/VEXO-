2d24a7a5f7553d5976dd25e7abfecb3e6939f915
harshanimationsanimatorharsh-ai <harshanimationsanimatorharsh@gmail.com>
2026-09-13 18:00:16 +0530

    Update DiscoverFragment.kt

--- app/src/main/java/com/vexo/app/ui/fragments/DiscoverFragment.kt
+++ app/src/main/java/com/vexo/app/ui/fragments/DiscoverFragment.kt
@@ -5,24 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.vexo.app.databinding.FragmentDiscoverBinding
+import com.vexo.app.R
 
 class DiscoverFragment : Fragment() {
-
-    private var _binding: FragmentDiscoverBinding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentDiscoverBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_discover, container, false)
     }
 }

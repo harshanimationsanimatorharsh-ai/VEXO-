6cdafbbf73c626ce74aed407ac1c253abae09e81
harshanimationsanimatorharsh-ai <harshanimationsanimatorharsh@gmail.com>
2026-09-15 12:48:58 +0530

    Add VexoApp class for application initialization

---
+++ app/src/main/java/com/vexo/app/ui/java/com/vexo/app/VexoApp.kt
@@ -0,0 +1,10 @@
+package com.vexo.app
+
+import android.app.Application
+import androidx.multidex.MultiDexApplication
+
+class VexoApp : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
